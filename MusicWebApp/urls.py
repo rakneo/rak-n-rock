1d476da05d4477a7ce80a747380471d569afbf77
rakneo <rv.g6598@outlook.com>
2016-11-06 22:09:14 +0530

    minor url update

--- MusicWebApp/urls.py
+++ MusicWebApp/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^music/',include('MusicApp.urls')),
+    url(r'',include('MusicApp.urls')),
 
 ]
 
