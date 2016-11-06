f221e56cb19eea4f69c02e16821d06b7412905ac
rakneo <rv.g6598@outlook.com>
2016-11-06 10:45:07 +0530

    Three Views Have been Added

--- MusicApp/views.py
+++ MusicApp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Album
 
-# Create your views here.
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', { 'all_albums' : all_albums })
+
+
+def detail(request, album_id):
+    #  album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album,pk=album_id)
+    return render(request,'music/detail.html', {'album': album})
+def favorite(request, is_fav):
+
+    return True
+
+
+
+
