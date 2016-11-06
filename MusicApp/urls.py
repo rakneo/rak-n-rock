b164870b844c5104fcd681fc2d00aaa218aee0b3
rakneo <rv.g6598@outlook.com>
2016-11-06 22:01:48 +0530

    now album thumbnail can be uploaded

--- MusicApp/urls.py
+++ MusicApp/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     url(r'^$',views.IndexView.as_view(),name="index"),
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
+    # /music/album/add/
+    url(r'^album/add$', views.AlbumCreate.as_view(), name='album_add'),
+    # /music/album/<pk>/
+    url(r'^album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album_update'),
+    # /music/album/<pk>/delete
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album_delete'),
 
 
 
--- MusicApp/views.py
+++ MusicApp/views.py
@@ -1,4 +1,6 @@
 from django.views import generic
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album, Song
 
 class IndexView(generic.ListView):
@@ -8,9 +10,28 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Album.objects.all()
+
 class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+    template_name = 'music/album_form.html'
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+    template_name = 'music/album_form.html'
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url =reverse_lazy('MusicApp:index')
+
+
+
+
 
 
 
