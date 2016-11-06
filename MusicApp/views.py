020b29f48fa9f8749ff0364dd3002053fb9cf071
rakneo <rv.g6598@outlook.com>
2016-11-06 15:39:54 +0530

    Lots of Code Done

--- MusicApp/views.py
+++ MusicApp/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Album
+from django.views import generic
+from .models import Album, Song
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request, 'music/index.html', { 'all_albums' : all_albums })
+class IndexView(generic.ListView):
+    model = Album
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
 
-def detail(request, album_id):
-    #  album = Album.objects.get(pk=album_id)
-    album = get_object_or_404(Album,pk=album_id)
-    return render(request,'music/detail.html', {'album': album})
-def favorite(request, is_fav):
 
-    return True
+
 
 
 
